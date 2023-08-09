3f03f071fee6eabbdd546b8bb4a7c1a930f3ec9c
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-08-09 10:45:53 +0600

    addeed sign in and signup function

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,45 @@
 import { Link } from 'react-router-dom';
 import svgImg from '../../assets/images/login/login.svg'
 import { FcGoogle } from "react-icons/fc";
+import { useContext } from 'react';
+import { AuthContext } from '../../provider/AuthProvider';
+import Swal from 'sweetalert2';
 
 const Login = () => {
+
+  const {signIn} = useContext(AuthContext)
+
+  const handleSignIn = e => {
+    e.preventDefault()
+    const form = e.target
+    const email = form.email.value
+    const password = form.password.value
+
+    console.log( email, password)
+
+    signIn(email, password)
+      .then(result => {
+        const user = result.user
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Your work has been saved',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      })
+      .catch(err => {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: 'Something Went Wrong!',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      })
+    
+  }
+  
   return (
     <div className="hero min-h-screen ">
       <div className="hero-content w-full flex items-center">
@@ -11,18 +48,18 @@ const Login = () => {
         </div>
         <div className="card  border border-[#D0D0D0] rounded w-full md:w-1/2">
           <h1 className='text-4xl font-semibold text-center mt-8'>Login In</h1>
-          <from className="card-body">
+          <form onSubmit={handleSignIn} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="text" placeholder="email" className="input input-bordered" />
+              <input type="email" name='email' placeholder="email" className="input input-bordered" />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="text" placeholder="password" className="input input-bordered" />
+              <input type="password" name='password' placeholder="password" className="input input-bordered" />
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
               </label>
@@ -30,12 +67,12 @@ const Login = () => {
             <div className="form-control mt-6">
               <button className="btn my-btn-primary">Login in</button>
             </div>
-          </from>
+          </form>
           <p className='text-center text-[18px] font-medium'>Or, Login with </p>
           <div className="">
             <FcGoogle className='text-center text-3xl mt-[30px] mb-[15px] mx-auto bg-[#F5F5F8] '/>
           </div>
-          <p className='text-[#737373] text-[18px] text-center mb-8'>Don't have an account? <Link to="/signUp" className='font-bold text-[#FF3811]'>Sign Up</Link></p>
+          <p className='text-[#737373] text-[18px] text-center mb-8'>Don&apos;t have an account? <Link to="/signUp" className='font-bold text-[#FF3811]'>Sign Up</Link></p>
         </div>
       </div>
     </div>
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,44 @@
 import { Link } from 'react-router-dom';
 import svgImg from '../../assets/images/login/login.svg'
 import { FcGoogle } from "react-icons/fc";
+import { useContext } from 'react';
+import { AuthContext } from '../../provider/AuthProvider';
+import Swal from 'sweetalert2';
 
 const SignUp = () => {
+  const {createUser} = useContext(AuthContext)
+
+  const handleSignUp = e => {
+    e.preventDefault()
+    const form = e.target
+    const name = form.name.value
+    const email = form.email.value
+    const password = form.password.value
+
+    console.log(name, email, password)
+    createUser(email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user)
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Successfully sign up!',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      })
+      .catch(err => {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: 'Something Went Wrong!',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      })
+  }
+  
   return (
     <div className="hero min-h-screen ">
       <div className="hero-content w-full flex items-center">
@@ -11,24 +47,24 @@ const SignUp = () => {
         </div>
         <div className="card  border border-[#D0D0D0] rounded w-full md:w-1/2">
           <h1 className='text-4xl font-semibold text-center mt-8'>Sign In</h1>
-          <from className="card-body">
+          <form onSubmit={handleSignUp} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
               </label>
-              <input type="text" placeholder="Name" className="input input-bordered" />
+              <input name='name' type="text" placeholder="Name" className="input input-bordered" />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="text" placeholder="email" className="input input-bordered" />
+              <input name='email' type="email" placeholder="email" className="input input-bordered" />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="text" placeholder="password" className="input input-bordered" />
+              <input name='password' type="password" placeholder="password" className="input input-bordered" />
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
               </label>
@@ -36,7 +72,7 @@ const SignUp = () => {
             <div className="form-control mt-6">
               <button className="btn my-btn-primary">Sign up</button>
             </div>
-          </from>
+          </form>
           <p className='text-center text-[18px] font-medium'>Or, sign up with </p>
           <div className="">
             <FcGoogle className='text-center text-3xl mt-[30px] mb-[15px] mx-auto bg-[#F5F5F8] ' />
